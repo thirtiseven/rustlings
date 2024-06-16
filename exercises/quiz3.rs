9796737ac3fe40418e139de933acb7af593e8be1
thirtiseven <ntlihy@gmail.com>
2024-06-16 21:28:41 +0800

    traits, lifetimes, quiz2, tests
    Signed-off-by: thirtiseven <ntlihy@gmail.com>

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,15 +16,20 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
-pub struct ReportCard {
-    pub grade: f32,
+pub struct ReportCard<T: ToString> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl ReportCard::<f32> {
+    pub fn print(&self) -> String {
+        format!("{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, &self.grade)
+    }
+}
+
+impl ReportCard::<&str> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
@@ -37,7 +42,7 @@ mod tests {
 
     #[test]
     fn generate_numeric_report_card() {
-        let report_card = ReportCard {
+        let report_card = ReportCard::<f32> {
             grade: 2.1,
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
@@ -51,8 +56,8 @@ mod tests {
     #[test]
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
-        let report_card = ReportCard {
-            grade: 2.1,
+        let report_card = ReportCard::<&str> {
+            grade: "A+",
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
